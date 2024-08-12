74ed5ff4d4498d67d48cdb49ddf5f5cbd4f15fd2
Victor Vahid <victorvahid76@example.com>
2024-08-12 12:26:10 -0400

    ✅

--- ex001.py
+++ ex001.py
@@ -11,6 +11,7 @@ else:
         print('Seu nome contém espaços')
     else:
         print('Seu nome não contém espaços')
+        
     print(f'Seu nome tem {len(nome)} letras')
     print(f'A primeira letra do seu nome é: {nome[0]}')
     print(f'A última letra do seu nome é: {nome[-1]}')
