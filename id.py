f33d60f65adf0db37cf0b5faa81e8e5951a5c4b6
Victor Vahid <victorvahid76@example.com>
2024-08-12 17:55:30 -0400

    ✅

---
+++ id.py
@@ -0,0 +1,30 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+
+"""
+
+#ID
+v1 = 'a'
+v2 = 'a'
+
+print(id(v1))
+print(id(v2))
+
+#Flag
+condicao = False
+passou_if = None
+
+if condicao:
+    passou_if = True
+    print('Faça algo!')
+else: 
+    print('Não faça algo')
+
+
+if passou_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
