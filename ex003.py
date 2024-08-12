69606fc5b2c149f2e6f239d8dc215182e531e5ab
Victor Vahid <victorvahid76@example.com>
2024-08-12 18:37:37 -0400

    ✅

--- ex003.py
+++ ex003.py
@@ -6,7 +6,9 @@ try:
         print('Bom dia')
     elif(int_horas >= 12) and (int_horas <=17):
         print('Boa tarde!')
-    else:
+    elif(int_horas >= 18)  and (int_horas <= 23):
         print('Boa noite!')
+    else:
+        print('Não conheço essa hora!')
 except:
-    print('Digite SOMENTE as Horas')
+    print('Digite somente números inteiros!')
