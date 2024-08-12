6a70e0a6a09f93949b5e43e78d9086bdc0d2a6d6
Victor Vahid <victorvahid76@example.com>
2024-08-12 18:42:06 -0400

    ✅

--- ex004.py
+++ ex004.py
@@ -1,11 +1,14 @@
 nome = input('Digite seu nome: ')
 
 try:
-    if(len(nome) <=4):
-        print('Seu nome é curto!')
-    elif(len(nome) >= 5) and (len(nome) <=6):
-        print('Seu nome é normal!')
+    if (len(nome)>1): 
+        if(len(nome) <=4):
+          print('Seu nome é curto!')
+        elif(len(nome) >= 5) and (len(nome) <=6):
+          print('Seu nome é normal!')
+        else:
+           print('Seu nome é muito grande!')
     else:
-        print('Seu nome é muito grande!')
+       print('Digite mais de uma letra!')
 except:
     print('Ocorreu um erro, digite seu nome!')
