27706280489c9c3e8d91175db333bd2c3f2be970
Victor Vahid <victorvahid76@example.com>
2024-08-12 12:41:02 -0400

    ✅

---
+++ constant.py
@@ -0,0 +1,25 @@
+
+"""
+CONSTANTE = "Variáveis" que não vão mudar
+Muitas condições no mesmo if (ruim)
+    <- Contagem de complexidade (ruim)
+"""
+velocidade = 61    #Velocidade do carro
+local_carro = 101   #Local em que o carro se encontra
+
+RADAR_1 = 60    # Velocidade máxima do radar 1
+LOCAL_1 = 100   # Local ONDE O radar 1 está
+RADAR_RANGE = 1 # A distancia onde o radar pega
+
+velocidade_radar1 = velocidade > RADAR_1
+passou_radar1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and local_carro <= (LOCAL_1 +RADAR_RANGE)
+carro_multado_radar1 = passou_radar1 and velocidade_radar1
+
+if velocidade_radar1:
+    print('Velocidade do carro passou do radar 1')
+
+if passou_radar1:
+    print('Carro passou radar 1')
+
+if carro_multado_radar1:
+    print('Carro foi multado no radar 1')
