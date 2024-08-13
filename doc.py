0068da42e006977298291d06cd85fb06fbcdc5c1
Victor Vahid <victorvahid76@example.com>
2024-08-12 20:01:33 -0400

    ✅

---
+++ doc.py
@@ -0,0 +1,10 @@
+"""
+
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+
+"""
+
+string = '1000'
+# print('Maria'.replace('ria','hid'))
+print(string.zfill(20))
