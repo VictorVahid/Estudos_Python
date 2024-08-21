489a04c3368cf235f9378e8f812dee8b0c6c3c8d
Victor Vahid <victorvahid76@example.com>
2024-08-21 16:13:47 -0400

    ✅

--- list.py
+++ list.py
@@ -16,7 +16,7 @@ Criar, ler, alterar, apagar = lista[i] (CRUD)
 """
 
 lista = [10, 20, 30, 40]
-lista.append('Victor Vahid')
+lista.append("Victor Vahid")
 nome = lista.pop()
 lista.append(12333)
 del lista[-1]
@@ -29,3 +29,24 @@ lista_b = [4, 5, 6]
 
 lista_a.extend(lista_b)
 print(lista_a)
+
+
+"""
+Lista de listas e seus índices
+
+"""
+
+
+salas = [
+    ["Maria","Helena",],
+
+    ["Elaine",],
+
+    ["Luiz","Flávio","Eduarda",],
+    
+    ]
+
+for sala in salas: 
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
