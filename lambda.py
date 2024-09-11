c46601b3cd84754a6befa4ac4dd450c405c9d8e9
Victor Vahid <victorvahid76@example.com>
2024-09-11 07:27:38 -0400

    ✅

--- lambda.py
+++ lambda.py
@@ -1,5 +1,62 @@
-salarios = [100, 200, 300, 400, 500]
+"""
+Introdução à função lambda (função anônima de uma linha)
+   A função lambda é uma função como qualquer
+   outra em Python. Porém, são funções anônimas
+   que contém apenas uma linha. Ou seja, tudo
+   deve ser contido dentro de uma única
+   expressão.
+lista = [
+        {'nome': 'Luiz', 'sobrenome': 'miranda'},
+        {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+        {'nome': 'Daniel', 'sobrenome': 'Silva'},
+        {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+        {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+"""
 
-# Uma variavel = uma lista(mapeamento de cada item(Função sem nome LAMBDA))
-novos_salario = list(map(lambda x:x * 1.1, salarios))
-print(novos_salario) 
+# lista = [1, 23, 433, 12, 23, 123, 15, 78, 878, 9]
+# lista.sort(reverse=True)
+# print(lista)
+
+lista = [
+    {"nome": "Luiz", "sobrenome": "miranda"},
+    {"nome": "Maria", "sobrenome": "Oliveira"},
+    {"nome": "Daniel", "sobrenome": "Silva"},
+    {"nome": "Eduardo", "sobrenome": "Moreira"},
+    {"nome": "Aline", "sobrenome": "Souza"},
+]
+
+# Ordenar por Função
+# def ordena(item):
+#     return item["nome"]
+
+# Ordenar por Lambda
+
+
+# lista.sort(key=lambda item: item["nome"])
+# def exibir(lista):
+#     for item in lista:
+#         print(item)
+#     print()
+
+
+l1 = sorted(lista, key=lambda item: item["nome"])
+l2 = sorted(lista, key=lambda item: item["sobrenome"])
+
+
+# exibir(l1)
+# exibir(l2)
+
+
+# Lambda em
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+duplica = executa(lambda m: lambda n: n * m, 2)
+
+print(duplica(5))
+
+# Execução de lambda em 1 única linha de código
+print(executa(lambda x, y: x * y, 2, 5))
+print(executa(lambda *args: sum(args), 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
