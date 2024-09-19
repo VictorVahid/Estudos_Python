dfdb3313ce7ec879ba06e71aa653f9045c4d2d52
Victor Vahid <victorvahid76@example.com>
2024-09-19 12:32:22 -0400

    ✅

--- id.py
+++ id.py
@@ -6,25 +6,25 @@ id = Identidade
 
 """
 
-#ID
-v1 = 'a'
-v2 = 'a'
+# ID
+v1 = "a"
+v2 = "a"
 
 print(id(v1))
 print(id(v2))
 
-#Flag
+# Flag
 condicao = False
 passou_if = None
 
 if condicao:
     passou_if = True
-    print('Faça algo!')
-else: 
-    print('Não faça algo')
+    print("Faça algo!")
+else:
+    print("Não faça algo")
 
 
 if passou_if is None:
-    print('Não passou no if')
+    print("Não passou no if")
 else:
-    print('Passou no if')
+    print("Passou no if")
