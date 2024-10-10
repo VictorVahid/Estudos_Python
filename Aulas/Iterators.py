1641267649b07606a58ed28d2023fab6a6aab8bb
Victor Vahid <victorvahid76@gmail.com>
2024-10-10 17:00:56 -0400

    ✅

--- Aulas/Iterators.py
+++ Aulas/Iterators.py
@@ -1,36 +1,15 @@
 # Generator expression, Iterables e Iterators em Python
 
 # Generator É UMA FUNÇÃO QUE PAUSA!
-import sys
+import sys as sys
 
 iterable = ["Eu", "Tenho", "__iter__"]
 iterator = iter(iterable)
 
-# print(next(iterator))
-# print(next(iterator))
-# print(next(iterator))
-
 lista = [n for n in range(1000000)]
 generator = (n for n in range(1000000))  # <-Declaração do generator
 
-# Diferença de Tamanho na Memória
-# print(sys.getsizeof(lista))
-# print(sys.getsizeof(generator))
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
 
-# for n in generator:
-#     print(n)
-
-
-def generator(n=0):
-    yield 1
-    return "Acabou"
-    yield 2
-    return "Continuando..."
-    yield 3
-    return "Acabou!"
-
-
-gen = generator(n=0)
-print(next(gen))
-print(next(gen))
-print(next(gen))
+print(generator)
