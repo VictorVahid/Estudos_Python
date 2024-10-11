46cfc92711e81e5c4a4718b9b3b565f567fbe51d
Victor Vahid <victorvahid76@gmail.com>
2024-10-10 20:10:56 -0400

    ✅

--- Aulas/aula99_package/__init__.py
+++ Aulas/aula99_package/__init__.py
@@ -1,2 +1,2 @@
-from aula99_package.modulesCalc import *
-from aula99_package.modulesB import *
+from aula99_package.modulesCalc import *  # noqa: F403
+from aula99_package.modulesB import *  # noqa: F403
